fd506922e25e02d89a37c0665fd82edf0d7524cf
AR <ardevelop01@gmail.com>
2024-01-29 21:46:43 +0800

    function tambah_data_asisten untuk meng-insert profile yang dibuat oleh asisten ke dalam database  mst_asisten

--- app/models/asisten_model.php
+++ app/models/asisten_model.php
@@ -21,4 +21,25 @@ class Asisten_model
         $this->db->bind('id', $id);
         return $this->db->single();
     }
+
+    public function tambah_data_asisten($data) {
+        $query = "INSERT INTO mst_asisten VALUES ('', :nama,  :kelas, '', '', :nim, :prodi, :angkatan, :email, :no_telp, :alamat, :deskripsi, :keahlian, :matkul)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama_lengkap']);
+        $this->db->bind('kelas', $data['kelas']);
+        $this->db->bind('nim', $data['nim']);
+        $this->db->bind('prodi', $data['prodi']);
+        $this->db->bind('angkatan', $data['angkatan']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('no_telp', $data['no_telp']);
+        $this->db->bind('alamat', $data['alamat']);
+        $this->db->bind('deskripsi', $data['deskripsi']);
+        $this->db->bind('keahlian', $data['keahlian']);
+        $this->db->bind('matkul', $data['matkul_praktikum']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
