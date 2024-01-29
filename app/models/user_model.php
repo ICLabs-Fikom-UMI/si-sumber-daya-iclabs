29d448b45a42aa99b1a9e53d8a56582eef901c9e
AR <ardevelop01@gmail.com>
2024-01-29 21:47:24 +0800

    hanya menambahkan function register dan  validasi role berdasarkan kode yang dimasukkan user. function ini belum berjalan

--- app/models/user_model.php
+++ app/models/user_model.php
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 <?php
 class User_model
 {
+    private $table = 'mst_user';
     private $db;
 
     public function __construct($db)
@@ -11,10 +11,45 @@ class User_model
 
     public function getUser($email, $password)
     {
-        $this->db->query("SELECT * FROM mst_user WHERE email = :email AND password = :password");
+        // $this->db->query('SELECT * FROM' . $this->table . ''  WHERE email = :email AND password = :password");
         $this->db->bind('email', $email);
         $this->db->bind('password', $password);
 
         return $this->db->resultSet();
     }
+
+    // signup mulai
+
+    public function registerUser($email, $password, $registrationCode)
+    {
+        // Validasi kode pendaftaran dan tentukan peran
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $kodePendaftaran = $_POST['kode_pendaftaran'];
+
+        $role = $this->validateRegistrationCode($registrationCode);
+
+        // Simpan data user ke dalam database (lakukan query INSERT)
+        $this->db->query('INSERT INTO ' . $this->table . '(email, password, role) VALUES (:email, :password, :role)"');
+        $this->db->bind(':email', $email);
+        $this->db->bind(':password', $password);
+        $this->db->bind(':role', $kodePendaftaran);
+
+        return $this->db->execute();
+    }
+
+    private function validateRegistrationCode($registrationCode)
+    {
+        // Tentukan peran berdasarkan kode pendaftaran
+        switch ($registrationCode) {
+            case 'K84PLZDJ':
+                return 'Kepala Lab';
+            case 'HFYJ7SW4':
+                return 'Laboran';
+            case 'BS4GW6EG':
+                return 'Asisten Lab';
+        }
+    }
+
+    // signup selesai
 }
