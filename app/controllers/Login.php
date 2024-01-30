34418b5e2ef40fc9161c1a5556c3cdb4c2c01e4c
AR <ardevelop01@gmail.com>
2024-01-30 15:28:52 +0800

    sudah bisa login dan sudah terdapat session berdasarkan role user. akan tetapi redirect halaman sesuai role belum bisa

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -12,34 +12,71 @@ class Login extends Controller
 
     public function login()
     {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        try {
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $email = $_POST['email'];
+                $password = $_POST['password'];
 
-        $role = $this->model('User_model')->getRoleByEmail($email);
+                $user = $this->model('User_model')->login($email, $password);
 
-        echo var_dump($_POST);
-        echo "<br></br>";
+                if ($user) {
+                    $role = $this->model('User_model')->getRoleByEmail($email);
 
-        $id_user = $this->model('User_model')->validateLogin($email, $password);
+                    // Set session user
+                    $_SESSION['user'] = [
+                        'id_user' => $user['id_user'],
+                        'email' => $user['email'],
+                        'role' => $role
+                    ];
 
-        if ($id_user) {
-            echo "Berhasil Login";
+                    // var_dump($_SESSION);
 
-            $is_password_default = $this->model('User_model')->isDefaultPassword($password);
-            // [PENTINGJ] hapus tanda (!) negasi untuk sementara untuk mengabaikan password default
-            if (!$is_password_default) {
-                // header('Location: change-default-password.php');
-                echo "Password Default";
-                exit();
-            } else {
-                $_SESSION['id_user'] = $id_user;
-                $_SESSION['role_user'] = $role['role'];
-                header('Location: ' . BASEURL . '/daftarNilai');
-                exit;
+                    // Redirect sesuai role
+                    switch ($role) {
+                        case 'Admin':
+                            header("Location: http://localhost:8080/tubesmvc/public/Signup");
+                            break;
+                        case 'Kepala Lab':
+                            header("Location: http://localhost:8080/tubesmvc/public/Profile_Asisten/detail_asisten");
+                            break;
+                        case 'Laboran':
+                            header("Location: http://localhost:8080/tubesmvc/public/Profile_Asisten/detail_asisten");
+                            break;
+                        case 'Asisten Lab':
+                            header("Location: http://localhost:8080/tubesmvc/public/Profile_Asisten/detail_asisten");
+                            break;
+                        default:
+                            // Redirect ke halaman default jika role tidak dikenali
+                            header("Location: http://localhost:8080/tubesmvc/public/Login");
+                            break;
+                    }
+                } 
             }
-        } else {
-            echo "Email Atau Password Salah";
+        } catch (Throwable $th) {
+            echo $th;
         }
+        
     }
 
+    // public function login()
+    // {
+    //     $email = $_POST['email'];
+    //     $password = $_POST['password'];
+
+    //     try {
+    //         $role = $this->model('User_model')->getRoleByEmail($email);
+    //         echo var_dump($_POST);
+    //         echo "<br></br>";
+
+    //         $id_user = $this->model('User_model')->validateLogin($email, $password);
+    //     } catch (Throwable $e) {
+    //         echo $e;
+    //     }
+
+    //     // $id_user = $this->model('User_model')->validateLogin($email, $password);
+
+    //     // if ($id_user) {
+    //     //     echo "Berhasil Login";
+    //     // }
+    // }
 }
