65495dfa1147c88af9f8926a8e6c2fa7c7d62e0b
AR <ardevelop01@gmail.com>
2024-02-03 13:17:03 +0800

    function register di rename menjadi function signup. Proses signup sudah bisa berjalan. user sudah bisa signup

--- app/controllers/Signup.php
+++ app/controllers/Signup.php
@@ -5,27 +5,30 @@ class Signup extends Controller
     public function index()
     {
         $data['judul'] = 'Signup';
+        // $data = $this->model('User_model')->registerUser())
         $this->view('templates/header_loginSignUp', $data);
         $this->view('templates/top_navbar_loginSignup');
         $this->view('signup/index', $data);
         $this->view('templates/footer');
     }
 
-    public function register () {
-        $userModel = $this->model('User_model');
-
-        // Validasi input
+    public function signup()
+    {
         $email = $_POST['email'];
-        $password = $_POST['password'];
-        $registrationCode = $_POST['kode_pendaftaran']; // Tambahkan ini sesuai dengan field input di formulir
+        $pass = $_POST['password'];
+        $kodePendaftaran = $_POST['kode_pendaftaran'];
 
-        // Hash password sebelum menyimpan ke database
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $role = $this->model('User_model')->cekKodeRegistrasi($kodePendaftaran);
 
-        // Register user dengan menentukan peran berdasarkan kode pendaftaran
-        $userModel->registerUser($email, $hashedPassword, $registrationCode);
+        if ($role == false){
+            header("Location: http://localhost:8080/tubesmvc/public/Signup");
+        } else {
+            $role = $this->model('User_model')->getRoleByEmail($email);
 
-        // Redirect ke halaman login setelah registrasi berhasil
-        echo 'berhasil';
+            
+
+            $this->model('User_model')->registerUser($email, $pass, $role);
+            header("Location: http://localhost:8080/tubesmvc/public/Login");
+        }
     }
 }
