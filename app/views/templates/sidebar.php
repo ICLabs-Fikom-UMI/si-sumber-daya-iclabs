9d3f616e6bc71fcee20b255c6710ff79d433c5d0
AR <ardevelop01@gmail.com>
2024-02-03 13:21:56 +0800

    sidebar admin ditambahkan

---
+++ app/views/templates/sidebar.php
@@ -0,0 +1,37 @@
+<div class="d-flex justify-content-start">
+    <div class="bg-white dibawah-topbar-rapat sidebar pt-4 ps-3 shadow vh-100 ">
+        <div class="container-fluid">
+            <h6 class="text-start fw-bol mb-3">Home</h6>
+            <ul style="margin-left: -1.8rem;">
+                <li style="list-style: none;">
+                    <a class="text-decoration-none text-black d-flex gap-3" href="">
+                        <i class="bi bi-house fw-normal"></i>
+                        <p class="fw-medium">Dashboard</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="container-fluid">
+            <h6 class="text-start fw-bol mb-3">Data Sumber Daya</h6>
+            <ul style="margin-left: -1.8rem;">
+                <li class="mb-2" style="list-style: none;">
+                    <a class="text-decoration-none d-flex text-black gap-3" href="">
+                        <i class="bi bi-person"></i>
+                        <p>Data Kepala Lab</p>
+                    </a>
+                </li>
+                <li class="mb-2" style="list-style: none;">
+                    <a class="text-decoration-none d-flex text-black gap-3" href="">
+                        <i class="bi bi-person"></i>
+                        <p>Data Laboran</p>
+                    </a>
+                </li>
+                <li class="mb-2" style="list-style: none;">
+                    <a class="text-decoration-none d-flex text-black gap-3" href="">
+                        <i class="bi bi-person"></i>
+                        <p>Data Asisten</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
