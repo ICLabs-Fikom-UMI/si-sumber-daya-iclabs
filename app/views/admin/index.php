b967357f032f90d5f244adcfb985a396ab833c19
AR <ardevelop01@gmail.com>
2024-02-03 13:14:39 +0800

    new view dashboard admin

---
+++ app/views/admin/index.php
@@ -0,0 +1,32 @@
+<div class="dibawah-topbar-rapat main-content pt-5 px-3">
+    <div class="container-fluid d-flex justify-content-between gap-3">
+        <div class="kotak-dashboard shadow p-3 rounded">
+            <div class="d-flex justify-content-between gap-4">
+                <h5 class="fw-bold">Total<br>Sumber Daya</h5>
+                <img class="person-image" src="../public/asset/image/icon/person-blue.png" alt="efdf">
+            </div>
+            <h5 class="fw-bold mt-4">40</h5>
+        </div>
+        <div class="kotak-dashboard shadow p-3">
+            <div class="d-flex justify-content-between gap-4">
+                <h5 class="fw-bold">Jumlah<br>Kepala Lab</h5>
+                <img class="person-image" src="../public/asset/image/icon/person-blue.png" alt="efdf">
+            </div>
+            <h5 class="fw-bold mt-4">15</h5>
+        </div>
+        <div class="kotak-dashboard shadow p-3">
+            <div class="d-flex justify-content-between gap-3">
+                <h5 class="fw-bold">Jumlah<br>Laboran</h5>
+                <img class="person-image" src="../public/asset/image/icon/person-blue.png" alt="efdf">
+            </div>
+            <h5 class="fw-bold mt-4">40</h5>
+        </div>
+        <div class="kotak-dashboard shadow p-3">
+            <div class="d-flex justify-content-between gap-3">
+                <h5 class="fw-bold">Jumlah<br>Asisten Lab</h5>
+                <img class="person-image" src="../public/asset/image/icon/person-blue.png" alt="efdf">
+            </div>
+            <h5 class="fw-bold mt-4">40</h5>
+        </div>
+    </div>
+</div>
