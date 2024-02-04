16a1da3db839f077fae7ba53c6d9579013df9452
AR <ardevelop01@gmail.com>
2024-02-04 20:42:56 +0800

    view telah diperbagus. telah ditambahkan juga datatables sehingga data yang ditampilkan lebih baik

---
+++ app/views/admin/data_laboran.php
@@ -0,0 +1,42 @@
+<div class="dibawah-topbar-rapat main-content pt-5 px-3">
+    <div class="container-fluid d-flex justify-content-between gap-3 mb-4">
+        <h4>Data Laboran</h4>
+    </div>
+    <a href="">
+        <button type="button" class="btn btn-primary ms-4 mb-4">Tambah Data</button>
+    </a>
+    <div class="container-fluid overflow-y-auto" style="max-height: 60vh;">
+        <table id="example" class="table table-striped mt-4" style="width:100%">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Jabatan</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                $i = 1;
+                foreach ($data['laboran'] as $laboran) : 
+                ?>
+                    <tr>
+                        <td><?= $i++ ?></td>
+                        <td><?= $laboran['id_sumber_daya'] ?></td>
+                        <td><?= $laboran['nama_laboran'] ?></td>
+                        <td>Laboran</td>
+                        <td >
+                            <a href="" >
+                                <img src="http://localhost:8080/tubesmvc/public/asset/image/icon/detail.png" alt="">
+                            </a>
+                            <a href="">
+                                <img src="http://localhost:8080/tubesmvc/public/asset/image/icon/delete.png" alt="">
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
