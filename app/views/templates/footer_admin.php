30f7e81e8f4237554fa4ba1c1e904d297d56f40b
AR <ardevelop01@gmail.com>
2024-02-04 17:58:48 +0800

    css dan javascript datatables telah ditambahkan. datatables sudah bisa tampil

--- app/views/templates/footer_admin.php
+++ app/views/templates/footer_admin.php
@@ -1,5 +1,11 @@
 </div>
 <script src="http://localhost:8080/tubesmvc/public/asset/js/bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    new DataTable('#example');
+</script>
 </body>
 
 </html>
