3191c0cae9aeb4bb77ed540c489915b2029e53ef
AR <ardevelop01@gmail.com>
2024-02-04 20:44:06 +0800

    button pada sidebar telah diarahkan sesuai dengan  view yang seharusnya tampil

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -1,10 +1,10 @@
 <div class="d-flex justify-content-start">
-    <div class="bg-white dibawah-topbar-rapat sidebar pt-4 ps-3 shadow vh-100 ">
+    <div class="bg-white dibawah-topbar-rapat sidebar pt-4 ps-3 shadow" style="height: 88vh;">
         <div class="container-fluid">
-            <h6 class="text-start fw-bol mb-3">Home</h6>
+            <h6 class="text-start fw-bold mb-3">Home</h6>
             <ul style="margin-left: -1.8rem;">
                 <li style="list-style: none;">
-                    <a class="text-decoration-none text-black d-flex gap-3" href="">
+                    <a class="text-decoration-none text-black d-flex gap-3" href="http://localhost:8080/tubesmvc/public/Dashboard/index">
                         <i class="bi bi-house fw-normal"></i>
                         <p class="fw-medium">Dashboard</p>
                     </a>
@@ -12,24 +12,24 @@
             </ul>
         </div>
         <div class="container-fluid">
-            <h6 class="text-start fw-bol mb-3">Data Sumber Daya</h6>
+            <h6 class="text-start fw-bold mb-3">Data Sumber Daya</h6>
             <ul style="margin-left: -1.8rem;">
                 <li class="mb-2" style="list-style: none;">
-                    <a class="text-decoration-none d-flex text-black gap-3" href="">
+                    <a class="text-decoration-none d-flex text-black gap-3" href="http://localhost:8080/tubesmvc/public/Dashboard/data_kepala_lab">
                         <i class="bi bi-person"></i>
-                        <p>Data Kepala Lab</p>
+                        <p class="fw-medium">Data Kepala Lab</p>
                     </a>
                 </li>
                 <li class="mb-2" style="list-style: none;">
-                    <a class="text-decoration-none d-flex text-black gap-3" href="">
+                    <a class="text-decoration-none d-flex text-black gap-3" href="http://localhost:8080/tubesmvc/public/Dashboard/data_laboran">
                         <i class="bi bi-person"></i>
-                        <p>Data Laboran</p>
+                        <p class="fw-medium">Data Laboran</p>
                     </a>
                 </li>
                 <li class="mb-2" style="list-style: none;">
-                    <a class="text-decoration-none d-flex text-black gap-3" href="">
+                    <a class="text-decoration-none d-flex text-black gap-3" href="http://localhost:8080/tubesmvc/public/Dashboard/data_asisten">
                         <i class="bi bi-person"></i>
-                        <p>Data Asisten</p>
+                        <p class="fw-medium">Data Asisten</p>
                     </a>
                 </li>
             </ul>
