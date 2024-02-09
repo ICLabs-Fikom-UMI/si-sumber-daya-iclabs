82194b69d3460a2f003597f13583c1d9525b1999
AR <ardevelop01@gmail.com>
2024-02-09 23:48:46 +0800

    refactor: mengubah link src yang telah menggunakan BASEURL

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -23,15 +23,15 @@
             <div class="card-info-box">
               <div class="center ms-0 fs-6">
                 <span class="fs-6">
-                  <img src="asset/image/icon/univ.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/univ.png" class="me-1">
                   <?= $kepalaLab['lulusan'] ?>
                 </span><br>
                 <span>
-                  <img src="asset/image/icon/nidn.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/nidn.png" class="me-1">
                   <?= $kepalaLab["nidn"] ?>
                 </span><br>
                 <span>
-                  <img src="asset/image/icon/prodi.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/prodi.png" class="me-1">
                   <?= $kepalaLab["dosen_prodi"] ?>
                 </span>
               </div>
@@ -70,11 +70,11 @@
             <div class="card-info-box">
               <div class="center ms-0 fs-6">
                 <span>
-                  <img src="asset/image/icon/univ.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/univ.png" class="me-1">
                   <?= $laboran['lulusan'] ?>
                 </span><br>
                 <span>
-                  <img src="asset/image/icon/calendar.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/calendar.png" class="me-1">
                   <?= $laboran['mulai_menjabat'] . ' - ' . $laboran['selesai_menjabat'] ?>
                 </span>
               </div>
@@ -114,7 +114,7 @@
             <div class="card-info-box">
               <div class="center ms-2 fs-6">
                 <span>
-                  <img src="asset/image/icon/univ.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/univ.png" class="me-1">
                   <?= $asisten['prodi']; ?>
                 </span><br>
                 <span>
@@ -122,11 +122,11 @@
                   <?= $asisten['kelas']; ?>
                 </span><br>
                 <span>
-                  <img src="asset/image/icon/nidn.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/nidn.png" class="me-1">
                   <?= $asisten['nim']; ?>
                 </span><br>
                 <span>
-                  <img src="asset/image/icon/calendar.png" class="me-1">
+                  <img src="<?= BASEURL ?>/asset/image/icon/calendar.png" class="me-1">
                   <?= $asisten['angkatan']; ?>
                 </span>
               </div>
